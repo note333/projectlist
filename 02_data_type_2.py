5cfc6931e1fefcefbb7954e9548c1e7adcbbd78e
note333 <note333@nate.com>
2017-02-16 20:48:21 +0900

    Add 02_data_type_2.py

---
+++ 02_data_type_2.py
@@ -0,0 +1,34 @@
+#리스트타입
+print("list")
+list_a=[1,2,3]
+print(list_a)
+
+# index=0부터시작
+print(list_a[0])
+print(list_a[1])
+print(list_a[2])
+
+print(type([1,2,3]))
+
+# slice
+print(list_a[0:2])
+
+# append
+list_a.append(4)
+print(list_a)
+
+# remove
+list_a.remove(2)
+print(list_a)
+
+#clear
+list_a.clear()
+print(list_a)
+
+
+#tuple타입
+print("tuple")
+tuple_a=(1,2,3)
+print(tuple_a)
+print(type(tuple_a))
+# tuple_a.append(4)
